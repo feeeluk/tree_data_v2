7ef2c4323f08bec2796956c3967b07ec951eaf7b
Phil Henning <philiphenning@outlook.com>
2026-05-09 20:40:14 +0100

    added basic login form

--- storage/framework/views/8ea164766ef9c3d521115d006f6aab9e.php
+++ storage/framework/views/8ea164766ef9c3d521115d006f6aab9e.php
@@ -2,28 +2,25 @@
 
 <?php $__env->startSection('page-content'); ?>
 
-<div>
+<div class="loginDiv">
 
     <form action="" class="loginForm">
 
         <table class="loginTable">
 
             <tr>
-                <td class="loginTableCol1">Userame:</td>
-                <td class="loginTableCol2"></td>
+                <td class="loginTableCol1">Username:</td>
                 <td class="loginTableCol3"><input type="text"></td>
             </tr>
 
             <tr>
                 <td class="loginTableCol1">Password:</td>
-                <td class="loginTableCol2"></td>
                 <td class="loginTableCol3"><input type="text"></td>
             </tr>
 
             <tr>
                 <td class="loginTableCol1"></td>
-                <td class="loginTableCol2"></td>
-                <td class="loginTableCol3"><button>Submit</button></td>
+                <td class="loginTableCol3"><input type="submit" value="Log in"><input type="button" value="Cancel"></td>
             </tr>
             
         </table>
