cc3435d84b426da7ff4761d04565f964a477c019
Phil Henning <philiphenning@outlook.com>
2026-05-13 20:28:05 +0100

    changed the routes again..

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,4 @@ use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
-Route::get('/test', fn() => view('pages.test'));
-
-// Route::post('/login', [LoginController::class, 'login'])->name('login');
-Route::get('/login', [LoginController::class, 'showLoginForm']);
-
+Route::post('/login', [LoginController::class, 'login'])->name('login');
